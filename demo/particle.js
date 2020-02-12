3a0f9e57440a508866fd83688a5544ee8ffbf484
lmtss <1224306181@qq.com>
2020-02-12 11:57:21 +0800

    u

---
+++ demo/particle.js
@@ -0,0 +1,13 @@
+import {DarkHole} from "../anim/DH-webgl2/index.js"
+
+
+
+var anim = new DarkHole();
+//document.body.insertBefore(anim.DOM(), document.getElementById('container'));
+
+anim.DOM().style.position = 'fixed';
+anim.DOM().style.top = '0';
+
+//document.body.insertBefore(anim.DOM(), document.getElementById('container'));
+document.body.append(anim.DOM());
+anim.play();
